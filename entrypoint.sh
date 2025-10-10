1e91119f720b6287083ef4d8f819f9099b048c2b
Hugo <weldu@yandex.com>
2025-10-10 15:58:44 +1300

    persist docker data

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-# Create data directory if it doesn't exist
-mkdir -p "${DATA_DIR:-/app/data}"
-
 # Run database migrations
 echo "Running database migrations..."
 alembic upgrade head
